b1d05e72b0a1b50a19a5a7c69879cec19d62f4df
DESKTOP-OF6976T\Carlos <carlossanmiguelbns@gmail.com>
2026-03-01 19:19:33 +0100

    Camera scene changes
    Creado suelo y "slime" de prueba.

--- Assets/_Scripts/_TEST/TestMovement.cs
+++ Assets/_Scripts/_TEST/TestMovement.cs
@@ -1,16 +1,36 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class TestMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private float movementSpeed=1f;
+
+    [SerializeField] 
+    private InputAction input;
+
+    private Rigidbody2D rb;
+    private float moveValue;
+    private void OnEnable()
     {
-        
+        input?.Enable();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveValue = input.ReadValue<float>();
+    }
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveValue* movementSpeed, rb.linearVelocityY);
+    }
+
+    private void OnDisable()
+    {
+        input?.Disable();
     }
 }
