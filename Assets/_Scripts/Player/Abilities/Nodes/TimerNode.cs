8f943973ecd81c79a4eb62ea52bfff3e0b4a9891
DESKTOP-OF6976T\Carlos <carlossanmiguelbns@gmail.com>
2026-04-20 17:44:23 +0200

    Abilities revamped
    Simple ability interface, node classification, ability context.

--- Assets/_Scripts/Player/Abilities/Nodes/TimerNode.cs
+++ Assets/_Scripts/Player/Abilities/Nodes/TimerNode.cs
@@ -1,7 +1,15 @@
+using System;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "ScriptableObjects/Ability Nodes/Timer Node")]
+[System.Serializable]
 public class TimerNode : AbilityNode
 {
- 
+    public override AbilityNodeCategory Category => AbilityNodeCategory.Flow;
+
+    public float time;
+
+    public override void Execute(AbilityContext context)
+    {
+
+    }
 }
