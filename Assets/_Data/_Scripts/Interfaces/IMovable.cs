2dafe5b49390f9dbd823f50ea7ad8a2900608d1f
notoier <oieralvarezp@gmail.com>
2026-06-01 18:37:13 +0200

    Movable Box

---
+++ Assets/_Data/_Scripts/Interfaces/IMovable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public interface IMovable
+{
+    Transform Transform { get; }
+    Rigidbody2D Rigidbody2D { get; }
+    float MoveSpeed { get; set; }
+    float WeightRequired { get; set; }
+
+    public void Move(Vector2 direction, Vector2? origin = null)
+    {
+        Vector2 position = origin ?? Transform.position;
+        // RaycastHit2D hit = Physics2D.Raycast(position, direction, 10f);
+        //
+        // Debug.DrawRay(position, direction, Color.red);
+        //
+        // if (hit.collider.name != this.Transform.gameObject.name) return; 
+
+        Vector2 velocity = Rigidbody2D.linearVelocity;
+
+        velocity.x = direction.normalized.x * MoveSpeed;
+
+        Rigidbody2D.linearVelocity = velocity;
+    }
+}
---
+++ Assets/_Data/_Scripts/LevelObjects/Box.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Unity.VersionControl.Git;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Box : MonoBehaviour, IMovable, IProvidesWeight
+{
+    public Transform Transform => transform;
+    public Rigidbody2D Rigidbody2D => GetComponent<Rigidbody2D>();
+    public float MoveSpeed { get; set; }= 2f;
+    public float WeightRequired { get; set; } = 2f;
+    public float Weight { get; set; } = 2f;
+    
+    private Rigidbody2D _rb;
+    
+    [Header("Debug")]
+    [SerializeField] private float debugWeight;
+    [SerializeField] private float debugWeightRequired;
+    [SerializeField] private float debugMoveSpeed;
+
+    private void Awake()
+    {
+        MoveSpeed = debugMoveSpeed;
+        WeightRequired = debugWeightRequired;
+        Weight = debugWeight;
+
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        
+        LockHorizontalMovement();
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        IProvidesWeight weightProvider = other.gameObject.GetComponent<IProvidesWeight>();
+        if (weightProvider == null) return;
+
+        if (weightProvider.Weight < WeightRequired)
+        {
+            LockHorizontalMovement();
+            return;
+        }
+
+        if (other.transform.position.y > this.transform.position.y * transform.lossyScale.y) return;
+        UnlockHorizontalMovement();
+
+        Vector2 dir = other.gameObject.GetComponent<CharacterMovement>()?.GetMovementDirection() ?? Vector2.zero;
+        ((IMovable)this).Move(dir);
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.GetComponent<IProvidesWeight>() == null) return;
+
+        LockHorizontalMovement();
+    }
+
+    private void LockHorizontalMovement()
+    {
+        _rb.linearVelocity = new Vector2(0f, _rb.linearVelocity.y);
+        _rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    private void UnlockHorizontalMovement()
+    {
+        _rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+}
