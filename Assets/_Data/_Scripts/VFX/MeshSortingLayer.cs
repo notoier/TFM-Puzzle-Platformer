a8d7bee04878cebde519b850ebdd75d92ad017fa
notoier <oieralvarezp@gmail.com>
2026-06-16 03:40:42 +0200

    Water bodies with physics & effects

---
+++ Assets/_Data/_Scripts/VFX/MeshSortingLayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+[RequireComponent(typeof(MeshRenderer))]
+public class MeshSortingLayer : MonoBehaviour
+{
+    [SerializeField] private string sortingLayerName = "Water";
+    [SerializeField] private int orderInLayer;
+
+    private MeshRenderer _meshRenderer;
+
+    private void Awake()
+    {
+        ApplySorting();
+    }
+
+    private void OnValidate()
+    {
+        ApplySorting();
+    }
+
+    private void ApplySorting()
+    {
+        if (!_meshRenderer)
+            _meshRenderer = GetComponent<MeshRenderer>();
+
+        _meshRenderer.sortingLayerName = sortingLayerName;
+        _meshRenderer.sortingOrder = orderInLayer;
+    }
+}
