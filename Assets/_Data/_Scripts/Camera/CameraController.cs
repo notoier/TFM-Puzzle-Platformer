8f6601f2382bb691d5fddeffef500e796aeed65f
Crihhh <cristinaclick14@gmail.com>
2026-06-17 18:34:04 +0200

    WallCheckFix and LookUp/Down

--- Assets/_Data/_Scripts/Camera/CameraController.cs
+++ Assets/_Data/_Scripts/Camera/CameraController.cs
@@ -9,14 +9,20 @@ public class CameraController : MonoBehaviour
     public float lookAheadDistance;
     public float smoothTimeX = 0.2f;
 
+    [Header("X")]
+    public float lookUpDistance;
+    public float smoothTimeY = 0.2f;
     [Header("Y")]
     public float verticalThreshold = 3f;
 
     private float currentOffsetX;
     private float velocityX;
 
+    private float currentOffsetY;
     private float baseY;
     private float velocityY;
+
+    private bool isLookingUp = false;
 
     private void Start()
     {
@@ -30,13 +36,24 @@ public class CameraController : MonoBehaviour
         Vector3 basePosition = player.position;
 
         //LOOKAHEAD (Moviemiento en X)
-        float inputX = GetInput();
+        float inputX = GetInputX();
 
         float targetOffsetX = inputX * lookAheadDistance;
         currentOffsetX = Mathf.SmoothDamp(currentOffsetX, targetOffsetX, ref velocityX, smoothTimeX);
 
         basePosition.x += currentOffsetX;
 
+        //LOOKUP/DOWN (Moviemiento en y)
+
+        float inputY = GetInputY();
+        if (isLookingUp)
+        { 
+            float targetOffsetY = inputY * lookUpDistance;
+            currentOffsetY = Mathf.SmoothDamp(currentOffsetY, targetOffsetY, ref velocityY, smoothTimeY);
+
+            basePosition.y += currentOffsetY;
+        }
+        
 
         //DEADZONE JUMP (Movimietno en y)
 
@@ -51,16 +68,20 @@ public class CameraController : MonoBehaviour
             baseY = player.position.y + verticalThreshold;
         }
 
-
-        basePosition.y = baseY;
+        if (!isLookingUp)
+        {
+            basePosition.y = baseY;
+        }
+            
         transform.position = basePosition;
 
         Debug.DrawLine(new Vector3(player.position.x - 5, baseY, 0), new Vector3(player.position.x + 5, baseY, 0), Color.red, 0.1f);
         Debug.DrawRay(new Vector3(player.position.x, baseY, 0), Vector3.up * 0.5f, Color.green);
     }
 
-    public float GetInput()
+    public float GetInputX()
     {
+        
         float value = 0f;
 
         if (Keyboard.current.aKey.isPressed)
@@ -75,4 +96,26 @@ public class CameraController : MonoBehaviour
         return value;
     }
 
+    public float GetInputY()
+    {
+        float value = 0f;
+
+        if (Keyboard.current.sKey.isPressed)
+        {
+            isLookingUp = true;
+            value -= 1;
+            Debug.Log("S");
+        }
+        else if (Keyboard.current.wKey.isPressed)
+        {
+            isLookingUp = true;
+            value += 1; ;
+            Debug.Log("w");
+        }
+        else
+        {
+            isLookingUp = false;
+        }
+        return value;
+    }
 }
