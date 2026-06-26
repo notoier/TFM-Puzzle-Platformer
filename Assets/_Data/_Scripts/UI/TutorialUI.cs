898aaaad3f5aa1ef04c7a130036b9cdf7c3dc846
Crihhh <cristinaclick14@gmail.com>
2026-06-26 15:37:57 +0200

    MainMenu

---
+++ Assets/_Data/_Scripts/UI/TutorialUI.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+
+public class TutorialUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject panel;
+    [SerializeField]
+    private TextMeshProUGUI text;
+
+    //public void show
+}
