6b8534ac3ff91c9aa35f99d0b1f6b6ff107c59f4
Crihhh <cristinaclick14@gmail.com>
2026-06-28 21:45:39 +0200

    MoreMainMenu

--- Assets/_Data/_Scripts/UI/Menu_Anim.cs
+++ Assets/_Data/_Scripts/UI/Menu_Anim.cs
@@ -1,19 +1,31 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Menu_Anim : MonoBehaviour
 {
     [SerializeField]
     private Animator slimeAnimator;
+    [SerializeField]
+    private Image slime;
 
+    private Color ogColor;
+
+    private void Awake()
+    {
+        ogColor = slime.color;
+
+    }
     public void Hover()
     {
         Debug.Log("Hover");
         slimeAnimator.SetBool("Hover", true);
+        slime.color = new Color32 (223, 144, 236, 255);
     }
     public void UnHover()
     {
         Debug.Log("Hover");
         slimeAnimator.SetBool("Hover", false);
+        slime.color = ogColor;
     }
 }
