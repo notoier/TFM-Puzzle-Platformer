45d983825d19855234e9d7b025802da1aec96a38
Crihhh <cristinaclick14@gmail.com>
2026-06-30 18:10:44 +0200

    PauseMenu

--- Assets/_Data/_Scripts/UI/PauseMenu.cs
+++ Assets/_Data/_Scripts/UI/PauseMenu.cs
@@ -2,32 +2,25 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour
+public class PauseMenu : UIPanel
 {
-
-    [SerializeField]
-    private GameObject pausePanel;
-
-    private bool IsPaused;
-
-    private void Toggle()
-    {
-        IsPaused = !IsPaused;
-        pausePanel.SetActive(IsPaused);
-        Time.timeScale = IsPaused ? 0 : 1;
-    }
 
     public void Resume()
     {
-        pausePanel.SetActive(false);
-        Time.timeScale = 1;
-        IsPaused = false;
+        GameManager.Instance.ResumeGame();
+    }
+    public void Options()
+    {
+        Debug.Log("Options");
     }
 
     public void ReturnMainMenu()
     {
-        Time.timeScale = 1;
-        SceneManager.LoadScene("MainMenu");
+        GameManager.Instance.LoadMainMenu();
+    }
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
 
